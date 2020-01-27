9f2359db423673b0506e29f452f472272f528ddd
Albert Lionelle <Albert.Lionelle@colostate.edu>
2020-01-27 15:27:08 -0700

    Added basic HelloWorld and .gitignore

---
+++ src/Unit1Examples/HelloWorld.java
@@ -0,0 +1,33 @@
+package Unit1Examples;
+
+/**
+ * <h1>Hello World Example</h1>
+ * This is a basic example of a first program printing out to the terminal
+ *
+ * @author Albert Lionelle <br>
+ *         lionelle@colostate.edu <br>
+ *         Computer Science Department <br>
+ *         Colorado State University
+ * @version 202010
+ */
+public class HelloWorld {
+
+
+    /**
+     * The main method is the entry point of the program, and the first thing called. Start following the code
+     * from this method.
+     * @param args these come from the comment line, and are unused in this program
+     */
+    public static void main(String[] args) {
+
+        String firstProgrammer = "Ada Lovelace";
+
+        System.out.println("Hello World");
+
+        System.out.print("Did you know that ");
+        System.out.print(firstProgrammer);
+        System.out.println(" was the worlds first programmer?");
+
+    }
+
+}
