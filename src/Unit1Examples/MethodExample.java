19e051429e6eb4532f17b809e5642984a36801db
Albert Lionelle <Albert.Lionelle@colostate.edu>
2020-01-30 10:29:56 -0700

    added showing a basic method

---
+++ src/Unit1Examples/MethodExample.java
@@ -0,0 +1,24 @@
+package Unit1Examples;
+
+/**
+ * <h1>Simple method examples</h1>
+ *
+ * @author Albert Lionelle <br>
+ *         lionelle@colostate.edu <br>
+ *         Computer Science Department <br>
+ *         Colorado State University
+ * @version 202010
+ */
+public class MethodExample {
+
+    public static String getName() {
+        return "Melody Pond";
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is " + getName());
+    }
+
+
+}
